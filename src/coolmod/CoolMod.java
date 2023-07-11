6912b1c97842b4d1bdc7692f04d7a032fa88eefa
Lime712 <122940036+Lime712@users.noreply.github.com>
2023-07-11 16:00:38 +1200

    Fixed?

--- src/coolmod/CoolMod.java
+++ src/coolmod/CoolMod.java
@@ -30,8 +30,8 @@ public class CoolMod extends Mod{
     @Override
     public void loadContent(){
         Log.info("Loading some cool content.");
-        content.CoolItems.load();
-        content.CoolBlocks.load();
+        coolmod.content.CoolItems.load();
+        coolmod.content.CoolBlocks.load();
     }
 
 }
